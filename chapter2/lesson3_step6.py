6df4e5fa18b333d5ef4343e87d49f8c1c47d096f
Egor <egsad2004@gmail.com>
2025-04-09 17:43:46 +0500

    chapter 2 lesson 3 step 4, 6: work with alert window and browser tab

---
+++ chapter2/lesson3_step6.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+try:
+    link = "https://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    browser.find_element(By.TAG_NAME, "button").click()
+
+    browser.switch_to.window(browser.window_handles[1])
+
+    x = int(browser.find_element(By.ID, "input_value").text)
+    y = math.log(abs(12 * math.sin(x)), math.e)
+
+    input = browser.find_element(By.ID, "answer")
+    input.send_keys(y)
+
+    browser.find_element(By.TAG_NAME, "button").click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
